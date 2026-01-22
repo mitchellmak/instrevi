ea9201b3e2d766cced6b88916fc75dc812dcba63
Mitchell <mitchell_mak@hotmail.com>
2026-01-22 18:13:41 +0800

    Fix CORS for PUT requests and increase JSON payload limit

--- server.js
+++ server.js
@@ -10,10 +10,13 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
     origin: ['http://192.168.18.71:8080', 'http://localhost:3000', 'http://127.0.0.1:8080', 'https://meethelp.sg', 'http://meethelp.sg'],
-    credentials: true
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.static(__dirname));
 
